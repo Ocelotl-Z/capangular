d480fa074f38f5aeff2e3ef54ce1dc5aebdca5c7
Ocelotl <ocelotlmagick@gmail.com>
2023-03-30 15:58:21 -0600

    Get

--- src/app/computers/computers.module.ts
+++ src/app/computers/computers.module.ts
@@ -4,14 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ComputersRoutingModule } from './computers-routing.module';
 import { ComputersComponent } from './computers.component';
 
+import { MatTableModule } from '@angular/material/table';
+import { NewComputersComponent } from './new-computers/new-computers.component';
 
 @NgModule({
-  declarations: [
-    ComputersComponent
-  ],
-  imports: [
-    CommonModule,
-    ComputersRoutingModule
-  ]
+  declarations: [ComputersComponent, NewComputersComponent],
+  imports: [CommonModule, ComputersRoutingModule, MatTableModule],
 })
-export class ComputersModule { }
+export class ComputersModule {}
