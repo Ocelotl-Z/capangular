84050426f6931c0d292c2860ca058d9391a86768
Ocelotl <ocelotlmagick@gmail.com>
2023-03-31 09:56:43 -0600

    Primeros TEST

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        HttpClientModule,
       ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -24,12 +36,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('capangular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('capangular app is running!');
   });
 });
--- src/app/services/login.service.spec.ts
+++ src/app/services/login.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoginService } from './login.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 describe('LoginService', () => {
   let service: LoginService;
+  let httpCtrl: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(LoginService);
+    httpCtrl = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get employees - success', () => {
+    service
+      .login({ email: 'eve.holt@reqres.in', password: 'cityslicka' })
+      .subscribe({
+        next: (response) => {
+          expect(response).toBeTruthy();
+        },
+      });
+    const mockHttp = httpCtrl.expectOne('http://localhost:3000/employees');
+    const httpRequest = mockHttp.request;
+
+    expect(httpRequest.method).toEqual('GET');
   });
 });
